a2113487a519293b2f547dcacabbc5400252c408
kanealdrich121 <kanealdrich12@gmail.com>
2022-12-03 18:33:37 -0500

    Working Version, Can Play Games but no Intelligent Move Choice

--- Move.py
+++ Move.py
@@ -9,56 +9,70 @@ class Move:
         if curDepth == 0:
             curLeaves.append(thisRoot)
             return
-        # Update Board
-        curBoard.push(thisRoot.thisMove)
+
         # Update History
         newHistory = thisRoot.historyToHere.copy()
         newHistory.append(thisRoot.thisMove)
 
-        for move in list(board.legal_moves):
+        for move in list(curBoard.legal_moves):
             # Make a Node for the Child Move
             node = MoveTree.Node(self.calculateMoveTotal(curBoard, move), newHistory, move, [])
             # Append the Child Node to Children
             thisRoot.children.append(node)
+            curBoard.push(move)
             self.createMoveTree(node, curDepth - 1, curBoard, curLeaves)
-        curBoard.pop()
+            curBoard.pop()
         return thisRoot, curLeaves
 
     def calculateMoveTotal(self, thisBoard, move):
-        pieceVals = {"P": 1, "N": 3, "B": 3, "R": 5, "Q": 8, "K": 10,
-                     "p": 1, "n": 3, "b": 3, "r": 5, "q": 8, "k": 10}
-        capture = thisBoard.is_capture(move)
-        gain = 0
-        if capture:
-            moveString = str(move)
-            attackedSquare = moveString[2:]
-            attackedPiece = board.piece_at(chess.parse_square(attackedSquare))
-            gain = pieceVals[str(attackedPiece)]
-        if not thisBoard.turn:
-            gain = -gain
-        return gain
+        # pieceVals = {"P": 1, "N": 3, "B": 3, "R": 5, "Q": 8, "K": 10,
+        #              "p": 1, "n": 3, "b": 3, "r": 5, "q": 8, "k": 10}
+        # print("This Board: ", thisBoard)
+        # print("This Move: ", move)
+        # capture = thisBoard.is_capture(move)
+        # gain = 0
+        # if capture:
+        #     moveString = str(move)
+        #     attackedSquare = moveString[2:]
+        #     attackedPiece = thisBoard.piece_at(chess.parse_square(attackedSquare))
+        #     gain = pieceVals[str(attackedPiece)]
+        # if not thisBoard.turn:
+        #     gain = -gain
+        # return gain
+        return 0
 
-    def getBestMove(self):
-        pass
+    def getBestMove(self, lastMove, curBoard):
+        # Check for Checkmate:
+        if curBoard.is_checkmate():
+            print("You Win! Good game")
+            exit()
+
+        # Generate Move Object for the Move Called thisMove (this is the last move played)
+        firstSquare = lastMove[:2]
+        secondSquare = lastMove[2:]
+        fromSquare = chess.parse_square(firstSquare)
+        toSquare = chess.parse_square(secondSquare)
+        thisMove = chess.Move(fromSquare, toSquare)
+
+        # Set Variables Needed
+        depth = 2
+        leaves = []
+
+        # Calculate the Root Node
+        thisRoot = MoveTree.Node(moveOperator.calculateMoveTotal(curBoard, thisMove), [], thisMove, [])
+
+        # Generate a Move Tree Starting from the Root Node (Board reflects last move played)
+        theseOptions, theseCurLeaves = moveOperator.createMoveTree(thisRoot, depth, curBoard, leaves)
+
+        # Find Best Move from Leaves
+        bestMove = None
+        bestEval = -9999
+        for leaf in theseCurLeaves:
+            if leaf.evalHere > bestEval:
+                bestEval = leaf.evalHere
+                bestMove = leaf.historyToHere[1]
+        return bestMove
+
 
 moveOperator = Move()
 board = chess.Board()
-thisMove = chess.Move(12, 28)
-depth = 2
-leaves = []
-# Setting Up Create Tree Call
-root = MoveTree.Node(moveOperator.calculateMoveTotal(board, thisMove), [], thisMove, [])
-options, curLeaves = moveOperator.createMoveTree(root, depth, board, leaves)
-bestMove = None
-bestEval = -9999
-for leaf in curLeaves:
-    if leaf.evalHere > bestEval:
-        bestEval = leaf.evalHere
-        bestMove = leaf.historyToHere[1]
-print(bestMove)
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import AI
 from Move import *
 
 board = chess.Board()
-print(Move().getBestMove())
 
-# while True:
-#     if board.turn:
-#         print(board)
-#         nextMove = input("Please enter a move: ")
-#         try:
-#             board.push_san(nextMove)
-#         except Exception as e:
-#             print('Bad Move: ', str(e))
-#     else:
+while True:
+    if board.turn:
+        print(board)
+        nextMove = input("Please enter a move: ")
+        try:
+            board.push_san(nextMove)
+        except Exception as e:
+            print('Bad Move: ', str(e))
+    else:
+        board.push(Move().getBestMove(nextMove, board))
+
+
 
